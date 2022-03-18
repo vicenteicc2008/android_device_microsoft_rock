c56a9f587b06c3669481e3cd37a02394e78849e4
Vicente Cortes <vicenteicc2008@gmail.com>
2022-03-18 17:13:08 -0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,8 +1,8 @@
-$(call inherit-product, device/motorola/otus/full_otus.mk)
+$(call inherit-product, device/nokia/rock/full_otus.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_otus
-PRODUCT_RELEASE_NAME := MOTO E 3G
+PRODUCT_NAME := cm_rock
+PRODUCT_RELEASE_NAME := Nokia Lumia 530
